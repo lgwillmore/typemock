244c911e89f1faed8c6c9a79c85909f46fae3707
laurence <laurence.willmore@magnetic.com>
2019-06-11 11:47:45 +0200

    snapshot version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="typemock",
-    version="0.0.1",
+    version="0.0.2-snapshot",
     author="Laurence Willmore",
     description="Type safe mocking",
     long_description=long_description,
