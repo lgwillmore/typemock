aafbb062b7b32e5cd9c7aee1373a98b3608e8015
laurence <laurence.willmore@magnetic.com>
2019-06-14 22:41:34 +0200

    python 3.5 and version tag

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
