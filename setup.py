c73fd1b910bb8ae11e1539276e2608929023355f
laurence <laurence.willmore@magnetic.com>
2019-06-16 15:26:40 +0200

    v0.3.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="typemock",
-    version="0.3.5",
+    version="0.3.6",
     author="Laurence Willmore",
     description="Type safe mocking",
     long_description=long_description,
