2eab8457e7a79a10aafde99b3665c0662f049f58
laurence <laurence.willmore@magnetic.com>
2019-06-19 08:26:13 +0200

    v0.4.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Laurence Willmore'
 author = 'Laurence Willmore'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.5'
+release = '0.4.0'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="typemock",
-    version="0.3.6",
+    version="0.4.0",
     author="Laurence Willmore",
     description="Type safe mocking",
     long_description=long_description,
