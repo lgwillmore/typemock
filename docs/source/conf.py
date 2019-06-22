bdc9d01ff2bb6bf9b82f85b841205651aa575012
laurence <laurence.willmore@magnetic.com>
2019-06-22 13:31:37 +0200

    V0.5.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Laurence Willmore'
 author = 'Laurence Willmore'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.3-SNAPSHOT'
+release = '0.5.0'
 
 
 # -- General configuration ---------------------------------------------------
